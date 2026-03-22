540da0c9401d8ce23cb91f14ceca0867f589b5e4
leiti20 <leiticiam3292@gmail.com>
2026-03-22 15:59:35 +0100

    update

--- services/evenement-service/main.py
+++ services/evenement-service/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"status": "ok", "message": "Service Python en ligne"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "UP"}
