67819bc7f5e7f0df85f49eed83cfef1f0062bd0e
leiti20 <leiticiam3292@gmail.com>
2026-03-28 09:24:06 +0100

    correction

--- services/vehicule-service/src/test/java/com/fleet/kafka/VehicleProducerTest.java
+++ services/vehicule-service/src/test/java/com/fleet/kafka/VehicleProducerTest.java
@@ -12,7 +12,6 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
